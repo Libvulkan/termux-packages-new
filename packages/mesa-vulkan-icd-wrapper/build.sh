13af75605632673d647e2932ed2ec30af5ab128e
Libvulkan <161806671+Libvulkan@users.noreply.github.com>
2025-01-22 17:16:54 +0000

    Update build.sh

--- packages/mesa-vulkan-icd-wrapper/build.sh
+++ packages/mesa-vulkan-icd-wrapper/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="Android Vulkan wrapper"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="xMeM <haooy@outlook.com>"
-TERMUX_PKG_VERSION="25.0.0"
 TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="25.0.0"
 TERMUX_PKG_SRCURL=git+https://github.com/Libvulkan/mesa
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libx11, libxcb, libxshmfence, libwayland, vulkan-loader-generic, zlib, zstd"
@@ -26,30 +26,31 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_post_get_source() {
-        git fetch --unshallow
-        # Do not use meson wrap projects
-        rm -rf subprojects
+	git fetch --unshallow
+
+	# Do not use meson wrap projects
+	rm -rf subprojects
 }
 
 termux_step_pre_configure() {
-        termux_setup_cmake
+	termux_setup_cmake
 
-        CPPFLAGS+=" -D__USE_GNU"
-        CPPFLAGS+=" -U__ANDROID__"
-        LDFLAGS+=" -landroid-shmem"
+	CPPFLAGS+=" -D__USE_GNU"
+	CPPFLAGS+=" -U__ANDROID__"
+	LDFLAGS+=" -landroid-shmem"
 
-        _WRAPPER_BIN=$TERMUX_PKG_BUILDDIR/_wrapper/bin
-        mkdir -p $_WRAPPER_BIN
-        if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-                sed 's|@CMAKE@|'"$(command -v cmake)"'|g' \
-                        $TERMUX_PKG_BUILDER_DIR/cmake-wrapper.in \
-                        > $_WRAPPER_BIN/cmake
-                chmod 0700 $_WRAPPER_BIN/cmake
-                termux_setup_wayland_cross_pkg_config_wrapper
-        fi
-        export PATH=$_WRAPPER_BIN:$PATH
+	_WRAPPER_BIN=$TERMUX_PKG_BUILDDIR/_wrapper/bin
+	mkdir -p $_WRAPPER_BIN
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		sed 's|@CMAKE@|'"$(command -v cmake)"'|g' \
+			$TERMUX_PKG_BUILDER_DIR/cmake-wrapper.in \
+			> $_WRAPPER_BIN/cmake
+		chmod 0700 $_WRAPPER_BIN/cmake
+		termux_setup_wayland_cross_pkg_config_wrapper
+	fi
+	export PATH=$_WRAPPER_BIN:$PATH
 }
 
 termux_step_post_configure() {
-        rm -f $_WRAPPER_BIN/cmake
+	rm -f $_WRAPPER_BIN/cmake
 }
